599e2e878f1d37e0cacf9b8114595a02081b2206
Ana Stanojevic <anastanojevic93@gmail.com>
2025-09-19 19:05:01 +0200

    model.py

---
+++ src/models.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import timm
+
+def create_model(model_name: str, num_classes: int, pretrained: bool = True):
+    name = model_name.lower()
+    if name in ["vit_tiny", "vit_tiny_patch16_224", "vit"]:
+        model = timm.create_model("vit_tiny_patch16_224", pretrained=pretrained, num_classes=num_classes)
+        img_size = 224
+    elif name in ["mobilenetv3_small", "mobilenet"]:
+        model = timm.create_model("mobilenetv3_small_100", pretrained=pretrained, num_classes=num_classes)
+        img_size = 224
+    else:
+        raise ValueError("Supported models: vit_tiny, mobilenetv3_small")
+    return model, img_size
