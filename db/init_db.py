5c2d0c95e6d8659bf35fcf2050fe00965d5b2e2d
Ana Stanojevic <anastanojevic93@gmail.com>
2026-01-18 13:37:45 +0100

    postgre database

--- db/init_db.py
+++ db/init_db.py
@@ -1,6 +1,6 @@
 from .base import Base
 from .session import engine
-from .models import Prediction  
+from .models import Prediction # noqa: F401
 
 def init_db():
     Base.metadata.create_all(bind=engine)
